c34fbe5342bfd3f30bede356bddbd58f23e3381d
Rifdatun Nafi'ah <149130538+rifdatnnfh@users.noreply.github.com>
2025-05-06 16:34:58 +0700

    update standard veginere chiper

--- app/app.py
+++ app/app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 import templates
 from routes.home import home_bp
 from routes.cipher_routes.autokey_route import autokey_bp
+from routes.cipher_routes.Route_Vigenere_standard import Vigenere_bp
 
 
 app = Flask(__name__, template_folder='templates')
 
 app.register_blueprint(home_bp)
 app.register_blueprint(autokey_bp)
-
+app.register_blueprint(Vigenere_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
