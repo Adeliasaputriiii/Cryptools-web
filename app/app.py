3e914469e7fb9e9904e33211a69c4c32214ee85e
Nuraisyah Safety Permata <nuraisyahsafetypermata04@gmail.com>
2025-05-07 10:51:41 +0700

    update hill cipher

--- app/app.py
+++ app/app.py
@@ -2,31 +2,19 @@ from flask import Flask, render_template
 import templates
 from routes.home import home_bp
 from routes.cipher_routes.autokey_route import autokey_bp
-<<<<<<< HEAD
 from routes.cipher_routes.affine_route import affine_bp
-=======
-<<<<<<< HEAD
 from routes.cipher_routes.playfair_route import playfair_bp
-=======
 from routes.cipher_routes.Route_Vigenere_standard import Vigenere_bp
->>>>>>> 46b0b698ee45abedcca6f2cf03a0c861cd303509
-
->>>>>>> 108ca88ad6f1d34b37417211432b0fa3f5aec421
+from routes.cipher_routes.hill_route import hill_bp
 
 app = Flask(__name__, template_folder='templates')
 
 app.register_blueprint(home_bp)
 app.register_blueprint(autokey_bp)
-<<<<<<< HEAD
 app.register_blueprint(affine_bp)
-=======
-<<<<<<< HEAD
 app.register_blueprint(playfair_bp)
->>>>>>> 108ca88ad6f1d34b37417211432b0fa3f5aec421
-
-=======
 app.register_blueprint(Vigenere_bp)
->>>>>>> 46b0b698ee45abedcca6f2cf03a0c861cd303509
+app.register_blueprint(hill_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
